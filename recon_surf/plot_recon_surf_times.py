2bd9544cd9aac96bfce4b6f55e5f28cf72298986
AhmedFaisal95 <ahmedfaisal_95@hotmail.co.uk>
2022-01-24 12:36:05 +0100

    Update normal plotting script with recent improvements

--- recon_surf/plot_recon_surf_times.py
+++ recon_surf/plot_recon_surf_times.py
@@ -16,12 +16,21 @@ def get_nonunique_cmd_execution_times(yaml_dicts, split_recon_all_stages=True):
     cmd_names = []
     cmd_times = []
     sides_list = []
+    subject_ids = []
 
     for yaml_dict in yaml_dicts:
         for stage_num in range(len(yaml_dict['recon-surf_commands'])):
             entry_list = list(yaml_dict['recon-surf_commands'][stage_num].values())[0]
 
             for cmd_entry in entry_list:
+                side = None
+                if any(lh_str in cmd_entry['cmd'] for lh_str in ['lh', '255 ']):
+                    side = 'lh'
+                elif any(rh_str in cmd_entry['cmd'] for rh_str in ['rh', '127 ']):
+                    side = 'rh'
+                else:
+                    side = 'full'
+
                 if cmd_entry['cmd'].split(' ')[0] == 'recon-all':
                     if not split_recon_all_stages:
                         cmd_names.append(cmd_entry['cmd'].split(' ')[0])
@@ -30,20 +39,18 @@ def get_nonunique_cmd_execution_times(yaml_dicts, split_recon_all_stages=True):
                         elif 'duration_s' in cmd_entry.keys():
                             cmd_times.append(cmd_entry['duration_s'])
 
-                    for stage_dict in cmd_entry['stages']:
-                        if split_recon_all_stages:
+                        sides_list.append(side)
+                        subject_ids.append(yaml_dict['sid'])
+
+                    else:
+                        for stage_dict in cmd_entry['stages']:
                             cmd_names.append('recon-all:'+stage_dict['stage_name'])
                             if 'duration_m' in stage_dict.keys():
                                 cmd_times.append(stage_dict['duration_m'])
                             elif 'duration_s' in stage_dict.keys():
                                 cmd_times.append(stage_dict['duration_s'])
-
-                        if any(lh_str in cmd_entry['cmd'] for lh_str in ['lh', '255 ']):
-                            sides_list.append('lh')
-                        elif any(rh_str in cmd_entry['cmd'] for rh_str in ['rh', '127 ']):
-                            sides_list.append('rh')
-                        else:
-                            sides_list.append('full')
+                            sides_list.append(side)
+                            subject_ids.append(yaml_dict['sid'])
 
                 else:
                     ## If python3 script, get script name:
@@ -57,14 +64,10 @@ def get_nonunique_cmd_execution_times(yaml_dicts, split_recon_all_stages=True):
                     elif 'duration_s' in cmd_entry.keys():
                         cmd_times.append(cmd_entry['duration_s'])
 
-                    if any(lh_str in cmd_entry['cmd'] for lh_str in ['lh', '255 ']):
-                        sides_list.append('lh')
-                    elif any(rh_str in cmd_entry['cmd'] for rh_str in ['rh', '127 ']):
-                        sides_list.append('rh')
-                    else:
-                        sides_list.append('full')
+                    sides_list.append(side)
+                    subject_ids.append(yaml_dict['sid'])
 
-    return cmd_names, cmd_times, sides_list
+    return cmd_names, cmd_times, sides_list, subject_ids
 
 def separate_hemis(filtered_df, sides_list):
     cols = filtered_df.columns.values.tolist()
@@ -74,6 +77,7 @@ def separate_hemis(filtered_df, sides_list):
     for index, row in filtered_df.iterrows():
         cmd_name = row['cmd_names']
         cmd_time = row['cmd_times']
+        subject_id = row['subject_id']
         side = sides_list[index]
 
         if 'recon-all' in cmd_name:
@@ -86,7 +90,7 @@ def separate_hemis(filtered_df, sides_list):
                 if cmd_name[-1] == ' ':
                     cmd_name = cmd_name[:-1]
 
-        rows_list.append([cmd_name, cmd_time, side])
+        rows_list.append([cmd_name, cmd_time, subject_id, side])
 
     two_sided_filtered_df = pd.DataFrame(rows_list, columns=cols)
 
@@ -199,9 +203,10 @@ if __name__ == "__main__":
 
     ## Extract recon-surf time information:
     print('[INFO] Extracting command execution times...')
-    cmd_names, cmd_times, sides_list = get_nonunique_cmd_execution_times(yaml_dicts, True)
+    cmd_names, cmd_times, sides_list, subject_ids = get_nonunique_cmd_execution_times(yaml_dicts, split_recon_all_stages=True)
 
-    df = pd.DataFrame({'cmd_names': cmd_names, 'cmd_times': cmd_times})
+    df = pd.DataFrame({'cmd_names': cmd_names, 'cmd_times': cmd_times, 'subject_id': subject_ids})
+
     filtered_df = df.copy()
 
     filtered_df = separate_hemis(filtered_df, sides_list)
